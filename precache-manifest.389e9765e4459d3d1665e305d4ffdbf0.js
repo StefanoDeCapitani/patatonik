f6ee22da1a5481a66909530b06f70158ba921a65
Stefano De Capitani <stefano.deca@live.com>
2022-02-20 11:23:38 +0100

    deploy

---
+++ precache-manifest.389e9765e4459d3d1665e305d4ffdbf0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5b89882e1d7979e582",
+    "url": "/patatonik/css/app.574f4f88.css"
+  },
+  {
+    "revision": "26140932157832f8c7ad71017f7dbb1b",
+    "url": "/patatonik/img/PATATONIK.26140932.png"
+  },
+  {
+    "revision": "886a1ff37551b7dc49b6591763689e8e",
+    "url": "/patatonik/img/background-patatonik.886a1ff3.png"
+  },
+  {
+    "revision": "8d5d33fffdba4a20616526075a6cf119",
+    "url": "/patatonik/img/pallina-di-palline-di-mais.8d5d33ff.png"
+  },
+  {
+    "revision": "4f83a1e315471698c9701da2000bf084",
+    "url": "/patatonik/img/patatina-logo.4f83a1e3.png"
+  },
+  {
+    "revision": "f11ebfb4f4e3603280ced969df21a159",
+    "url": "/patatonik/img/slider-knob.f11ebfb4.svg"
+  },
+  {
+    "revision": "75990eddf496ebc65503902eecc1e837",
+    "url": "/patatonik/img/slider-track.75990edd.svg"
+  },
+  {
+    "revision": "9ea3adeb755787746d1d185ffa9b2d99",
+    "url": "/patatonik/img/wheel-background.9ea3adeb.svg"
+  },
+  {
+    "revision": "8d1a2d76c5733f5d4a5f3e6da7cad3b8",
+    "url": "/patatonik/img/wheel.8d1a2d76.svg"
+  },
+  {
+    "revision": "da9dd113f0c6ca6aaffbd50da9555d62",
+    "url": "/patatonik/index.html"
+  },
+  {
+    "revision": "1f5b89882e1d7979e582",
+    "url": "/patatonik/js/app.d84d1500.js"
+  },
+  {
+    "revision": "451a2d9621528ddb1ce0",
+    "url": "/patatonik/js/chunk-vendors.8ea7b523.js"
+  },
+  {
+    "revision": "28e169c13400cb16f5094ff3f8f10d89",
+    "url": "/patatonik/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/patatonik/robots.txt"
+  }
+]);
